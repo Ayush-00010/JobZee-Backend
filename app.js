7b00e952bf86733819f350f00660013fc043c2f3
Ayush-00010 <146408867+Ayush-00010@users.noreply.github.com>
2024-06-19 14:52:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL,'https://job-zee-frontend-sigma.vercel.app'],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
